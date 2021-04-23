c533c7466164491a3e833a7df8511cad70a49db3
matt0852 <matthew.rocketmail@gmail.com>
2021-04-24 00:20:27 +0800

    Now runs on Flask

--- bot.py
+++ bot.py
@@ -1,9 +1,20 @@
 from wiki_parser import get_all_links
 from link_crawler import generate_links
+import flask
+from flask import jsonify, request
 
-title = 'Philosophy'
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
 
-for _ in range(20):
-    output = generate_links(title)[0]
-    print(output)
-    title = output
+@app.route('/', methods = ['GET'])
+def return_links():
+    if 'title' in request.args:
+        title = request.args['title']
+    else:
+        return 'Bad request', 400
+
+    return jsonify(
+        title = title,
+        links = generate_links(title))
+
+app.run()
